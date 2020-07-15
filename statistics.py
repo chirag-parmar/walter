7fb2bd1a2b3090b5b29f643cf4c36563067d6ea3
chirag-parmar <chirag.parmar@slock.it>
2020-07-15 23:09:19 +0200

    use plotly for graphing the datasets

--- statistics.py
+++ statistics.py
@@ -2,6 +2,8 @@ import json
 import numpy as np
 import sys
 
+import plotly.graph_objects as go
+
 def moving_average(a, n=3) :
     ret = np.cumsum(a, dtype=float)
     ret[n:] = ret[n:] - ret[:-n]
@@ -9,7 +11,7 @@ def moving_average(a, n=3) :
 
 if len(sys.argv) < 2:
     print("Usage: python3 statistics.py <path_to_dataset>.json")
-    print("Options: -f include filling=true datapoints")
+    print("Options: -m compute moving average of datapoints corresponding to a level")
     exit()
 
 dataset_file = open(sys.argv[1],)
@@ -35,14 +37,45 @@ for datapoint in dataset:
 
     dataset_matrix[level].append(value)
 
+mean_array = []
+level_array = []
+error_array = []
+error_minus_array = []
+sd_array = []
+
 for lvl in dataset_matrix:
     if "-m" in sys.argv:
         mean = np.mean(moving_average(dataset_matrix[lvl], 10))
-        sd = np.std(moving_average(dataset_matrix[lvl], 10))
-        print("Level {} -> Mean: {}, SD: {}".format(lvl, mean, sd))
+        mean_array.append(mean)
+        sd_array.append(np.std(moving_average(dataset_matrix[lvl], 10)))
+        error_array.append(np.max(moving_average(dataset_matrix[lvl], 10)) - mean)
+        error_minus_array.append(mean - np.min(moving_average(dataset_matrix[lvl], 10)))
     else:
         mean = np.mean(dataset_matrix[lvl])
-        sd = np.std(dataset_matrix[lvl])
-        print("Level {} -> Mean: {}, SD: {}".format(lvl, mean, sd))
+        mean_array.append(mean)
+        sd_array.append(np.std(dataset_matrix[lvl]))
+        error_array.append(np.max(dataset_matrix[lvl]) - mean)
+        error_minus_array.append(mean - np.min(dataset_matrix[lvl]))
+
+    level_array.append(int(lvl))
+
+#display the graph
+fig = go.Figure(data=go.Scatter(
+        x=level_array,
+        y=mean_array,
+        error_y=dict(
+            type='data',
+            symmetric=False,
+            array=error_array,
+            arrayminus=error_minus_array
+            ),
+        xaxis='x',
+        yaxis='y'
+        ))
+
+fig.update_xaxes(type="linear")
+fig.update_yaxes(type="linear")
+
+fig.show()
 
 
